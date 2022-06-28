f54a3c4afc9e250b44a2bdf21d55cca8fb8e3acb
Chung Tran <bigearsenal@gmail.com>
2022-06-28 12:06:00 +0700

    feat: add demo splash when opening demo

--- Example/KeyAppUIExample/ViewController.swift
+++ Example/KeyAppUIExample/ViewController.swift
@@ -21,6 +21,8 @@ class ViewController: UIViewController {
         child.autoPinEdgesToSuperviewEdges()
 
         view.backgroundColor = UIColor(red: 0.91, green: 0.92, blue: 0.95, alpha: 1)
+        
+        addSplash()
     }
 
     func build() -> UIView {
@@ -51,5 +53,25 @@ class ViewController: UIViewController {
     private func presentSplash() {
         let splashVC = SplashViewController()
         present(splashVC, animated: true)
+    }
+    
+    private func addSplash() {
+        let child = SplashViewController()
+        child.completionHandler = {[weak self, weak child] in
+            guard let self = self, let child = child else {return}
+            self.removeSplash(child)
+        }
+        
+        addChild(child)
+
+        view.addSubview(child.view)
+        child.view.autoPinEdgesToSuperviewEdges()
+        child.didMove(toParent: self)
+    }
+
+    private func removeSplash(_ vc: SplashViewController) {
+        vc.willMove(toParent: nil)
+        vc.view.removeFromSuperview()
+        vc.removeFromParent()
     }
 }
--- Sources/KeyAppUI/Splash/SplashViewController.swift
+++ Sources/KeyAppUI/Splash/SplashViewController.swift
@@ -2,15 +2,17 @@ import UIKit
 import BEPureLayout
 
 public final class SplashViewController: BEViewController {
+    
+    public var completionHandler: (() -> Void)? {
+        get { customView.completionHandler }
+        set { customView.completionHandler = newValue }
+    }
 
     private let customView = SplashView()
 
     public override func setUp() {
         self.view.addSubview(customView)
         customView.autoPinEdgesToSuperviewEdges()
-        customView.completionHandler = { [weak self] in
-            self?.dismiss(animated: true)
-        }
     }
 
     public override func viewDidAppear(_ animated: Bool) {
