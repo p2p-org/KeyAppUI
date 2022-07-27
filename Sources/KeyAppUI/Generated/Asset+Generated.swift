7e1f0a8088b88c14aa4b1104e94d134c9a27b6a8
Tran Giang Long <trangianglong4003@gmail.com>
2022-07-27 09:19:39 +0200

    feat(pincode): add

--- Sources/KeyAppUI/Generated/Asset+Generated.swift
+++ Sources/KeyAppUI/Generated/Asset+Generated.swift
@@ -56,11 +56,13 @@ public enum Asset {
     // swiftlint:enable trailing_comma
   }
   public enum Icons {
+    public static let remove = ImageAsset(name: "remove")
     public static let send = ImageAsset(name: "send")
     // swiftlint:disable trailing_comma
     public static let allColors: [ColorAsset] = [
     ]
     public static let allImages: [ImageAsset] = [
+      remove,
       send,
     ]
     // swiftlint:enable trailing_comma
