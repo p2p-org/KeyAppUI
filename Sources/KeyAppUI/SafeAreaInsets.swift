e231528ed151a67011bbab9b4374cab294f2d41f
Tran Giang Long <trangianglong4003@gmail.com>
2022-09-01 13:19:55 +0200

    feat(safe-area): change scope

--- Sources/KeyAppUI/SafeAreaInsets.swift
+++ Sources/KeyAppUI/SafeAreaInsets.swift
@@ -17,19 +17,19 @@ extension UIApplication {
     }
 }
 
-private struct SafeAreaInsetsKey: EnvironmentKey {
-    static var defaultValue: EdgeInsets {
+public struct SafeAreaInsetsKey: EnvironmentKey {
+    static public var defaultValue: EdgeInsets {
         UIApplication.shared.keyWindow?.safeAreaInsets.swiftUiInsets ?? EdgeInsets()
     }
 }
 
-extension EnvironmentValues {
+public extension EnvironmentValues {
     var safeAreaInsets: EdgeInsets {
         self[SafeAreaInsetsKey.self]
     }
 }
 
-private extension UIEdgeInsets {
+public extension UIEdgeInsets {
     var swiftUiInsets: EdgeInsets {
         EdgeInsets(top: top, leading: left, bottom: bottom, trailing: right)
     }
